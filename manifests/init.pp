a9045f01f446dd3a0ea48146216e0fbc39e999a2
Ubuntu <ubuntu@ip-172-31-4-237.ec2.internal>
2022-11-30 14:35:33 +0000

    init module elasticsearch

---
+++ manifests/init.pp
@@ -0,0 +1,105 @@
+class elasticsearchv8 {
+
+  class { 'java':
+    distribution => jre,
+    #before => Class['::elasticsearch'],  Class['::logstash']],
+  }
+  
+  yumrepo { 'elasticsearch':
+    ensure     => 'present',
+    descr      => 'Elasticsearch repository for 8.x packages',
+    gpgcheck   => '1',
+    enabled    => '1',
+    gpgkey     => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+    baseurl    => 'https://artifacts.elastic.co/packages/8.x/yum',
+  }
+  ->
+  package {'elasticsearch':
+    ensure => installed,
+    require => Class['java']
+  }
+  ->
+  #elasticsearch.yaml configuration
+  file {'/etc/elasticsearch/elasticsearch.yml':
+    ensure => file,
+    mode   => '0660',
+    owner  => 'root',
+    group  => 'elasticsearch',
+    source => 'puppet:///modules/elasticsearchv8/elasticsearch.yml',
+  }
+  ->
+  service {'elasticsearch':
+    ensure => running,
+    enable => true,
+    require => Package['elasticsearch']
+  }
+  
+  ->
+  
+  #metricbeat
+  package {'metricbeat':
+    ensure  => installed,
+    require => Package['elasticsearch'],
+  }
+  ~>
+  service {'metricbeat':
+    ensure => running,
+    enable => true,
+  }
+  ->
+  #logstash
+  package {'logstash':
+    ensure  => installed,
+    require => Package['elasticsearch'],
+  }
+  ->
+  file {'/etc/logstash/conf.d/central.conf':
+    ensure => file,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+    source => 'puppet:///modules/elasticsearchv8/central.conf',
+  }
+
+  ~>
+  service {'logstash':
+    ensure => running,
+    enable => true,
+  }
+  ->
+  #kibana
+  package {'kibana':
+    ensure  => installed,
+    require => Package['elasticsearch'],
+  }
+  ->
+  user {'kibana':
+    ensure => present,
+    shell  => '/bin/false',
+    before => [File['kibana.service'], Service['kibana']],
+  }
+  ->
+  file {'kibana.service':
+    path   => '/usr/lib/systemd/system/kibana.service',
+    ensure => file,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'kibana',
+    source => 'puppet:///modules/elasticsearchv8/kibana.service',
+  }
+  ->
+  file {'kibana.yaml':
+    path   => '/etc/kibana/kibana.yml',
+    ensure => file,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'kibana',
+    source => 'puppet:///modules/elasticsearchv8/kibana.yml',
+  }
+  ~>
+  service {'kibana':
+    ensure => running,
+    enable => true,
+  }
+
+}
