bd620b1217b5ac42a3019c39c65034d1864c84fa
ciokma <53948541+ciokma@users.noreply.github.com>
2022-11-30 09:00:29 -0600

    only elasticsearch

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,6 @@ class elasticsearchv8 {
 
   class { 'java':
     distribution => jre,
-    #before => Class['::elasticsearch'],  Class['::logstash']],
   }
   
   yumrepo { 'elasticsearch':
@@ -32,74 +31,6 @@ class elasticsearchv8 {
     ensure => running,
     enable => true,
     require => Package['elasticsearch']
-  }
-  
-  ->
-  
-  #metricbeat
-  package {'metricbeat':
-    ensure  => installed,
-    require => Package['elasticsearch'],
-  }
-  ~>
-  service {'metricbeat':
-    ensure => running,
-    enable => true,
-  }
-  ->
-  #logstash
-  package {'logstash':
-    ensure  => installed,
-    require => Package['elasticsearch'],
-  }
-  ->
-  file {'/etc/logstash/conf.d/central.conf':
-    ensure => file,
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'root',
-    source => 'puppet:///modules/elasticsearchv8/central.conf',
-  }
-
-  ~>
-  service {'logstash':
-    ensure => running,
-    enable => true,
-  }
-  ->
-  #kibana
-  package {'kibana':
-    ensure  => installed,
-    require => Package['elasticsearch'],
-  }
-  ->
-  user {'kibana':
-    ensure => present,
-    shell  => '/bin/false',
-    before => [File['kibana.service'], Service['kibana']],
-  }
-  ->
-  file {'kibana.service':
-    path   => '/usr/lib/systemd/system/kibana.service',
-    ensure => file,
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'kibana',
-    source => 'puppet:///modules/elasticsearchv8/kibana.service',
-  }
-  ->
-  file {'kibana.yaml':
-    path   => '/etc/kibana/kibana.yml',
-    ensure => file,
-    mode   => '0644',
-    owner  => 'root',
-    group  => 'kibana',
-    source => 'puppet:///modules/elasticsearchv8/kibana.yml',
-  }
-  ~>
-  service {'kibana':
-    ensure => running,
-    enable => true,
   }
 
 }
